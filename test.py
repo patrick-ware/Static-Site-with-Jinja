0a2bb03a31ee124364acf6c70c1cf34cc56981a5
Patrick Ware <patrick.r.ware@gmail.com>
2020-04-05 12:43:21 -0700

    test updated to include more .os functions, next step is to insert pages[] list into build.py

--- test.py
+++ test.py
@@ -1,32 +1,36 @@
 import glob
 import os
 
-#all_html_files = glob.glob("./content/*.html")
-#print(all_html_files)
+# all_html_files = glob.glob("./content/*.html")
+# print(all_html_files)
 
-#file_path = "./content/aboutme.html"
-#file_name = os.path.basename(file_path)
-#print(file_name)
-#name_only, extension = os.path.splitext(file_name)
-#print(name_only)
+# file_path = "./content/aboutme.html"
+# file_name = os.path.basename(file_path)
+# print(file_name)
+# name_only, extension = os.path.splitext(file_name)
+# print(name_only)
 
-#pages_auto=[]
-#all_html_files = glob.glob("./content/*.html")
-#for page in all_html_files:
-#    filename = os.path.basename(page)
-#    name_only, extension = os.path.splitext(filename)
-#    pages_auto.append({
-#        'filename': filename,
-#        'output': "./docs/" + filename,
-#        'title': name_only.title(),
-#        'image_display': '',
-#    })
+pages = []
+all_html_files = glob.glob("./content/*.html")
+for page in all_html_files:
+    rel_path = os.path.relpath(page)
+    orig_dir = os.path.dirname(page)
+    output_dir = './docs/'
+    name_only, extension = os.path.splitext(file_name)
+    pages.append({
+        'filename': rel_path
+        'output': os.path.join(output_dir, name_only, extension),
+        'title': name_only,
+        'image_display': '',
+    })
 
-#print(pages_auto)
+print(pages)
+
 
 def read_page(filename):
     content = open(filename).read()
     return content
+
 
 page_text = read_page("./content/resume.html")
 
