f77962f005bb19411c6b17c3b6d593d97449fbb1
Patrick Ware <patrick.r.ware@gmail.com>
2020-04-10 19:04:39 -0700

    adding sys commands

--- manage.py
+++ manage.py
@@ -1,2 +1,17 @@
 import utils
-utils.main()
+import sys
+
+print("This is argv:", sys.argv)
+command = sys.argv[0]
+while command != "build" and command != "new":
+    command = input("Please specify 'build' or 'new':")
+
+if command == "build":
+    print("Build was specified")
+    utils.main()
+elif command == "new":
+    print("New page was specified")
+    title = input("Choose a title for new page:")
+
+# need to add a create an "add page" func to utils
+
